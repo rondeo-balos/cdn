f805e5166bcd5733f08c3cd478b02a879877bcf3
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2022-07-13 16:49:05 +0800

    Create autoplay-vimeo.js

---
+++ autoplay-vimeo.js
@@ -0,0 +1,22 @@
+jQuery('document').ready(function($){
+        //autoplay video
+        playVimeo_init($);
+    });
+
+    function playVimeo_init($){
+        var player = $('iframe[src*="player.vimeo.com"]');
+        console.log(player);
+        if(player.length <= 0)
+            playVimeo_init($);
+        else
+            playVimeo($, player);
+    }
+
+    function playVimeo($, player){
+        var url = new URL($(player).attr('src'));
+        url.searchParams.set('autoplay', '1');
+        url.searchParams.set('loop', 1);
+        $(player).attr('src',url);
+        //remove cover
+        $('.n2_ss_video_player__cover img').remove();
+    }
